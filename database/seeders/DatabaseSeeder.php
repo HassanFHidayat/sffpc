d39465ae3e9291b583d94e2bae75a78a5a21791b
HassanFHidayat <44991223+HassanFHidayat@users.noreply.github.com>
2022-08-22 14:39:05 +0700

    Database Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use App\Models\TingkatKepentingan;
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -20,5 +22,22 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        TingkatKepentingan::Create([
+            'pembeli_id' => '1',
+            'cpu_lokal' => '3',
+            'gpu_lokal' => '5',
+            'ram_lokal' => '3',
+            'storage_lokal' => '2',
+            'ssd_lokal' => '4',
+            'hdd_lokal' => '2',
+            'harga_lokal' => '5',
+            'cpu_global' => '0.166666667',
+            'gpu_global' => '0.277777778',
+            'ram_global' => '0.166666667',
+            'ssd_global' => '0.074074074',
+            'hdd_global' => '0.037037037',
+            'harga_global' => '0.277777778'
+        ]);
     }
 }
